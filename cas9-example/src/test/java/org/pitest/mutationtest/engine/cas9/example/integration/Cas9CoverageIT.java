82bb392821c7192d606f5e99bc11c4b4d8eda998
Glauco Aquino <glauco.aquino@aurea.com>
2020-10-24 09:51:48 -0300

    fix(example) add it for sbr mutations

--- cas9-example/src/test/java/org/pitest/mutationtest/engine/cas9/example/integration/Cas9CoverageIT.java
+++ cas9-example/src/test/java/org/pitest/mutationtest/engine/cas9/example/integration/Cas9CoverageIT.java
@@ -5,18 +5,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.util.Map;
+import javax.xml.bind.JAXBException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Cas9CoverageIT {
 
-  @Test
-  void shouldGenerateExpectedCoverageReport() throws Exception {
+  private Map<String, Map<String, Long>> coverage;
+
+  @BeforeEach
+  void setUp() throws Exception {
     final File file = new File(System.getProperty("pitest.report.file"));
-    final Map<String, Map<String, Long>> coverage = Mutations.loadFromXml(file);
+    coverage = Mutations.loadFromXml(file);
+  }
+
+  @Test
+  void shouldGenerateExpectedCoverageReport() {
     assertAll(
         () -> assertEquals(1, coverage.get("AOR").get("KILLED")),
         () -> assertEquals(4, coverage.get("ROR").get("KILLED")),
         () -> assertEquals(3, coverage.get("ROR").get("SURVIVED")),
         () -> assertEquals(3, coverage.get("UOI").get("SURVIVED")));
+  }
+
+  @Test
+  void shouldGenerateExpectedSbrMutationCoverageReport() {
+    assertAll(
+        () -> assertEquals(2, coverage.get("SBR").get("KILLED")),
+        () -> assertEquals(2, coverage.get("SBR").get("SURVIVED")),
+        () -> assertEquals(1, coverage.get("SBR").get("RUN_ERROR")));
   }
 }
